3ebeb2ee33d38931112d47f9ca1b68878ba739fd
phsoo <91250611+smwater@users.noreply.github.com>
2022-08-22 14:59:34 +0900

    MoveObject 코드 리팩토링

--- Assets/Scripts/Interactive Object/MoveObject.cs
+++ Assets/Scripts/Interactive Object/MoveObject.cs
@@ -5,23 +5,11 @@ using UnityEngine;
 public class MoveObject : MonoBehaviour
 {
     private float _distance = 2.5f;
-    private Rigidbody _rigidbody;
-
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.useGravity = true;
-    }
 
     private void OnMouseDrag()
     { 
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _distance);
         Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = objectPosition;
-    }
-
-    private void OnMouseUp()
-    {
-        _rigidbody.useGravity = true;
     }
 }
