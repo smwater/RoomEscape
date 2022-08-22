80d55579c493c0dada7746728059715a9dd3ff56
phsoo <91250611+smwater@users.noreply.github.com>
2022-08-22 18:09:13 +0900

    서랍장과 티비장의 문 각각 열릴 수 있게 구현

--- Assets/Scripts/Interactive Object/Door.cs
+++ Assets/Scripts/Interactive Object/Door.cs
@@ -8,6 +8,7 @@ public class Door : MonoBehaviour, IInteractable
     private float _timer;
     private Animator _animator;
     private bool _onoff;
+    private int _doorNum;
 
     private void Awake()
     {
@@ -17,9 +18,14 @@ public class Door : MonoBehaviour, IInteractable
         _outline.OutlineColor = Color.magenta;
         _outline.OutlineWidth = 5f;
 
-        _animator = GetComponent<Animator>();
-
         Inactive();
+
+        _animator = GetComponentInParent<Transform>().GetComponentInParent<Animator>();
+
+        if (_animator == null)
+        {
+            _animator = GetComponentInParent<Transform>().GetComponentInParent<Transform>().GetComponentInParent<Animator>();
+        }
     }
 
     private void FixedUpdate()
@@ -46,6 +52,11 @@ public class Door : MonoBehaviour, IInteractable
     public void Interact()
     {
         string keyword = "open";
+        if (gameObject.GetComponent<DoorNumber>() != null)
+        {
+            _doorNum = gameObject.GetComponent<DoorNumber>().Number;
+            keyword += _doorNum.ToString();
+        }
 
         _onoff = !_onoff;
 
