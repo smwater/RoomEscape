91250e130b371da8734609c0394f8bf9d9e6f89f
phsoo <smwater130@naver.com>
2022-08-25 16:30:08 +0900

    아이템 자세히 보기가 열려있는 동안의 움직임 막음

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,6 +6,8 @@ public class PlayerMove : MonoBehaviour
 {
     public Transform HeadTransform;
 
+    private bool _isMovement;
+
     private float _moveSpeed = 5f;
     private float _positionX;
     private float _positionZ;
@@ -21,27 +23,53 @@ public class PlayerMove : MonoBehaviour
     private void Awake()
     {
         _input = GetComponent<PlayerInput>();
+        _isMovement = true;
+    }
+
+    private void OnEnable()
+    {
+        GameManager.Instance.ViewItemDetail.AddListener(MovementOff);
+        GameManager.Instance.CloseItemDetail.AddListener(MovementOn);
     }
 
     private void Update()
     {
-        _positionX = _input.X * _moveSpeed * Time.deltaTime;
-        _positionZ = _input.Y * _moveSpeed * Time.deltaTime;
-
-        transform.Translate(_positionX, 0f, _positionZ, Space.Self);
-
-        if (_input.RightClick)
+        if (_isMovement)
         {
-            _rotateMoveY = _input.MouseMoveX * _rotateSpeed * Time.deltaTime;
-            _rotateMoveX = -_input.MouseMoveY * _rotateSpeed * Time.deltaTime;
+            _positionX = _input.X * _moveSpeed * Time.deltaTime;
+            _positionZ = _input.Y * _moveSpeed * Time.deltaTime;
 
-            _rotateX += _rotateMoveX;
-            _rotateY = transform.eulerAngles.y + _rotateMoveY;
+            transform.Translate(_positionX, 0f, _positionZ, Space.Self);
 
-            _rotateX = Mathf.Clamp(_rotateX, -90, 90);
+            if (_input.RightClick)
+            {
+                _rotateMoveY = _input.MouseMoveX * _rotateSpeed * Time.deltaTime;
+                _rotateMoveX = -_input.MouseMoveY * _rotateSpeed * Time.deltaTime;
 
-            transform.eulerAngles = new Vector3(0f, _rotateY, 0f);
-            HeadTransform.eulerAngles = new Vector3(_rotateX, _rotateY, 0f);
+                _rotateX += _rotateMoveX;
+                _rotateY = transform.eulerAngles.y + _rotateMoveY;
+
+                _rotateX = Mathf.Clamp(_rotateX, -90, 90);
+
+                transform.eulerAngles = new Vector3(0f, _rotateY, 0f);
+                HeadTransform.eulerAngles = new Vector3(_rotateX, _rotateY, 0f);
+            }
         }
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.ViewItemDetail.RemoveListener(MovementOff);
+        GameManager.Instance.CloseItemDetail.RemoveListener(MovementOn);
+    }
+
+    public void MovementOn()
+    {
+        _isMovement = true;
+    }
+
+    public void MovementOff()
+    {
+        _isMovement = false;
     }
 }
