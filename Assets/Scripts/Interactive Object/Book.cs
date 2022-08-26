310bfe9c25c7d6f6c3d6cfac75b6ee263ec8cecb
phsoo <smwater130@naver.com>
2022-08-26 12:09:59 +0900

    아이템 창이 켜져있는데도 책이 움직여지는 버그 일부 수정
    - 지금 책을 가리키는 방식이 ray 방식이 아니라서 인식범위 제한이 걸리지 않는다. 추후 수정할 것.

--- Assets/Scripts/Interactive Object/Book.cs
+++ Assets/Scripts/Interactive Object/Book.cs
@@ -49,9 +49,12 @@ public class Book : MonoBehaviour, IInteractable
 
     private void OnMouseDrag()
     {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _distance);
-        Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = objectPosition;
-        transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        if (GameManager.Instance.PlayerCanMovement)
+        {
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _distance);
+            Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objectPosition;
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        }
     }
 }
