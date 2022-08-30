9295ea863e66159c9905210ac86fda9d1ed6590c
phsoo <smwater130@naver.com>
2022-08-30 17:54:33 +0900

    암호 키패드 구현 완료

---
+++ Assets/Scripts/Interactive Object/Lock/Button.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour, IInteractable
+{
+    public enum Type
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Confirm,
+        Erase
+    };
+
+    public Type Name;
+
+    private NumberLock _numberLock;
+    private Outline _outline;
+    private float _timer;
+    private readonly float _timeLimit = 0.1f;
+
+    private void Awake()
+    {
+        _numberLock = GetComponentInParent<NumberLock>();
+
+        _outline = gameObject.AddComponent<Outline>();
+
+        _outline.OutlineMode = Outline.Mode.OutlineAll;
+        _outline.OutlineColor = Color.magenta;
+        _outline.OutlineWidth = 5f;
+
+        Inactive();
+    }
+
+    private void FixedUpdate()
+    {
+        _timer += Time.fixedDeltaTime;
+
+        if (_timer >= _timeLimit)
+        {
+            Inactive();
+            _timer = 0;
+        }
+    }
+
+    public void Active()
+    {
+        _outline.enabled = true;
+    }
+
+    public void Inactive()
+    {
+        _outline.enabled = false;
+    }
+
+    public void Interact()
+    {
+        if (Name == Type.Confirm)
+        {
+            _numberLock.Confirm();
+            return;
+        }
+
+        if (Name == Type.Erase)
+        {
+            _numberLock.Erase();
+            return;
+        }
+
+        _numberLock.InputNumber = (int)Name;
+    }
+}
--- Assets/Scripts/Interactive Object/Lock/NumberLock.cs
+++ Assets/Scripts/Interactive Object/Lock/NumberLock.cs
@@ -1,13 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberLock : MonoBehaviour
 {
     public bool IsLock { get; private set; }
+    public string Password;
+    [HideInInspector] public int InputNumber;
+    public TextMeshPro InsertText;
+
+    private int[] _inputPassword = new int[4];
+    private int _blankNumberIndex;
+    private string _inputedPassword;
+    private bool _onErrorLog;
 
     private void Awake()
     {
         IsLock = true;
+        InputNumber = -1;
+        _blankNumberIndex = 0;
+        _onErrorLog = false;
+    }
+
+    private void Update()
+    {
+        if (InputNumber != -1 && _blankNumberIndex <= 3 && !_onErrorLog)
+        {
+            _inputPassword[_blankNumberIndex] = InputNumber;
+            _blankNumberIndex++;
+            InputNumber = -1;
+
+            _inputedPassword = ArrayToString();
+
+            InsertText.text = _inputedPassword;
+        }
+        else
+        {
+            InputNumber = -1;
+        }
+    }
+
+    private string ArrayToString()
+    {
+        string password = "";
+
+        for (int i = 0; i < _inputPassword.Length; i++)
+        {
+            if (_inputPassword[i] == 0)
+            {
+                password += "0";
+                continue;
+            }
+            
+            password += _inputPassword[i].ToString();
+        }
+
+        return password;
+    }
+
+    public void Confirm()
+    {
+        if (Password.Equals(_inputedPassword))
+        {
+            InsertText.text = "Success";
+            IsLock = false;
+        }
+        else
+        {
+            StartCoroutine(Error());
+
+            for (int i = 0; i < _inputPassword.Length; i++)
+            {
+                _inputPassword[i] = 0;
+            }
+            _blankNumberIndex = 0;
+        }
+    }
+
+    private IEnumerator Error()
+    {
+        _onErrorLog = true;
+        InsertText.text = "Error";
+        yield return new WaitForSeconds(1f);
+        InsertText.text = "0000";
+        _onErrorLog = false;
+    }
+
+    public void Erase()
+    {
+        if (_blankNumberIndex <= 0)
+        {
+            return;
+        }
+
+        _inputPassword[--_blankNumberIndex] = 0;
+
+        _inputedPassword = ArrayToString();
+
+        InsertText.text = _inputedPassword;
     }
 }
