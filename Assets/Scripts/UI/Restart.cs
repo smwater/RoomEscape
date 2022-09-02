1f6ee4c888015570ea87a6ead89b4d86d0bb7b6c
phsoo <smwater130@naver.com>
2022-09-02 16:38:59 +0900

    일시정지 화면에서 게임 다시 시작하기 구현

---
+++ Assets/Scripts/UI/Restart.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void Click()
+    {
+        GameManager.Instance.PlayerCanMovement = true;
+        SceneManager.LoadScene(1);
+    }
+}
